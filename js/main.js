1034dcd1bb5fac9f58c4405c06d8edd85a7f9339
desoga <done2stay@gmail.com>
2018-09-14 14:51:24 +0100

    added click event listener

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-var myRequest = new XMLHttpRequest();
-myRequest.open('GET', 'https://jsonplaceholder.typicode.com/users');
-myRequest.onload = function() {
-  console.log(myRequest.responseText);
-};
-myRequest.send();
+var btn = document.getElementById('button');
+
+btn.addEventListener('click', function() {
+  var myRequest = new XMLHttpRequest();
+  myRequest.open('GET', 'https://jsonplaceholder.typicode.com/users');
+  myRequest.onload = function() {
+    var myData = JSON.parse(myRequest.responseText);
+    console.log(myData[0]);
+  };
+  myRequest.send();
+});
